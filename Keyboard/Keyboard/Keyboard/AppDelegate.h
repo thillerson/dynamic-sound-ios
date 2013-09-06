45da2a8b9fede7d2c8b5e4b89d043041c01f946f
Tony Hillerson <tony.hillerson@gmail.com>
2013-09-05 21:28:11 -0600

    It's all a keyboard or something

---
+++ Keyboard/Keyboard/Keyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Keyboard
+//
+//  Created by Tony Hillerson on 9/5/13.
+//  Copyright (c) 2013 Programming Sound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Keyboard/Keyboard/Keyboard/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Keyboard
+//
+//  Created by Tony Hillerson on 9/5/13.
+//  Copyright (c) 2013 Programming Sound. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)keyDown:(id)sender {
+    
+}
+
+- (IBAction)keyUp:(id)sender {
+    
+}
+
+@end
